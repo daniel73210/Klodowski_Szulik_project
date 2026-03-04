2f1df02754ecf4308317611389f434ba7e0bd829
dklodowski@student.agh.edu.pl <dklodowski@student.agh.edu.pl>
2026-03-04 21:45:24 +0100

    calculate_proc_change

--- data_preparation.py
+++ data_preparation.py
@@ -3,7 +3,16 @@ import pandas as pd
 def match_price_dates(data_frame_1: pd.DataFrame, data_frame_2: pd.DataFrame):
     df1 = data_frame_1.copy()
     df2 = data_frame_2.copy()
+    df1 = df1[['date', 'open', 'close']]
+    df2 = df2[['date', 'open', 'close']]
     df1["date"] = pd.to_datetime(df1["date"])
     df2["date"] = pd.to_datetime(df2["date"])
 
     return pd.merge(df1, df2, on="date", how="inner", suffixes=('_1', '_2'))
+
+def calculate_proc_change(data_frame: pd.DataFrame):
+    temp = data_frame.copy()
+    temp["proc_change_1"] = (temp["close_1"] / temp["open_1"] - 1) * 100
+    temp["proc_change_2"] = (temp["close_2"] / temp["open_2"] - 1) * 100
+
+    return temp
