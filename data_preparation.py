9cd1933130729ce7d7af15319b212b864a52981a
dklodowski@student.agh.edu.pl <dklodowski@student.agh.edu.pl>
2026-03-04 21:28:42 +0100

    match_price_dates function

--- data_preparation.py
+++ data_preparation.py
@@ -1,1 +1,10 @@
 import pandas as pd
+
+def match_price_dates(data_frame_1: pd.DataFrame, data_frame_2: pd.DataFrame):
+    df1 = data_frame_1.copy()
+    df2 = data_frame_2.copy()
+    df1["date"] = pd.to_datetime(df1["date"])
+    df2["date"] = pd.to_datetime(df2["date"])
+
+    temp = pd.merge(df1, df2, on="date", how="inner", suffixes=('_1', '_2'))
+    return temp
