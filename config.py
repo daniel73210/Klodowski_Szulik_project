a484f39b00c952f64cce6b3815a7ab81537ad36c
dklodowski@student.agh.edu.pl <dklodowski@student.agh.edu.pl>
2026-03-05 14:20:30 +0100

    config SYMBOLS

---
+++ config.py
@@ -0,0 +1,4 @@
+CATEGORIES = {"commodities": ('historical-price-eod/full', ['BZUSD', 'SIUSD', 'GCUSD']),
+           "forex": ('historical-price-eod/full', ['EURUSD', 'USDJPY', 'GBPUSD', 'USDCHF', 'USDCAD', 'USDCNH', 'USDRUB', 'USDPLN']),
+           "crypto": ('historical-price-eod/full', ['BTCUSD', 'ETHUSD', 'USDTUSD', 'USDCUSD', 'SOLUSD', 'XRPUSD', 'BNBUSD']),
+           "indexes": ('historical-price-eod/full', ['^GSPC', '^DJI', '^IXIC', '^RUT', '^FTSE', '^N225', '^HSI', '^STOXX50E', '^VIX'])}
