367cbd1922dc20ceb64b935e4c112ce15f47f1a4
dklodowski@student.agh.edu.pl <dklodowski@student.agh.edu.pl>
2026-03-05 15:23:09 +0100

    changed conception, and added new function in data_preparation.py named: prepare_price_df

--- data_preparation.py
+++ data_preparation.py
@@ -1,29 +1,18 @@
 import pandas as pd
 
-def match_price_dates(data_frame_1: pd.DataFrame, data_frame_2: pd.DataFrame):
+def prepare_price_df(extra_df: pd.DataFrame) -> pd.DataFrame:
     """
-    Matches price dates between two dataframes and removes useless columns
-    :param data_frame_1:
-    :param data_frame_2:
-    :return: new dataFrame
+    Counts the price change, converts "date" to datetime, removes unnecessary columns and adds a multiindex
+    :param extra_df: contains data to be connected to the main dataframe
+    :return: modified extra_df
     """
-    df1 = data_frame_1.copy()
-    df2 = data_frame_2.copy()
-    df1 = df1[['date', 'open', 'close']]
-    df2 = df2[['date', 'open', 'close']]
-    df1["date"] = pd.to_datetime(df1["date"])
-    df2["date"] = pd.to_datetime(df2["date"])
+    symbol = extra_df["symbol"].iloc[0]
+    df = extra_df.copy()
+    df["date"] = pd.to_datetime(df["date"])
+    df["perc_change"] = df["close"].pct_change()
 
-    return pd.merge(df1, df2, on="date", how="inner", suffixes=('_1', '_2'))
+    df = df[["date", "close", "perc_change"]]
 
-def calculate_proc_change(data_frame: pd.DataFrame):
-    """
-    Adds percent change in prices for two instruments
-    :param data_frame: dataFrame processed by match_price_dates function
-    :return: new dataFrame with proc_change column
-    """
-    temp = data_frame.copy()
-    temp["proc_change_1"] = (temp["close_1"] / temp["open_1"] - 1) * 100
-    temp["proc_change_2"] = (temp["close_2"] / temp["open_2"] - 1) * 100
+    df.columns = [c if c == "date" else (symbol, c) for c in df.columns]
 
-    return temp
+    return df.set_index("date")
